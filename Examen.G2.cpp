97e9351a8efd4c155e4354422142f9c83c9fc400
Pul287 <71336629@continental.edu.pe>
2025-06-09 22:08:20 -0500

    Update Examen.G2.cpp

--- Examen.G2.cpp
+++ Examen.G2.cpp
@@ -16,3 +16,22 @@ struct Proceso{
     }
 };
 
+Proceso* frente = NULL;
+
+void encolarProceso(int id, int tiempo, int prioridad) {
+    Proceso* nuevo = new Proceso(id, tiempo, prioridad);
+
+    if (frente == NULL || prioridad < frente->prioridad) {
+        nuevo->siguiente = frente;
+        frente = nuevo;
+    } else {
+        Proceso* actual = frente;
+        while (actual->siguiente != NULL && actual->siguiente->prioridad <= prioridad) {
+            actual = actual->siguiente;
+        }
+        nuevo->siguiente = actual->siguiente;
+        actual->siguiente = nuevo;
+    }
+
+    cout << "Proceso numero" << id << " encolado - prioridad: " << prioridad << endl;
+}
